7dd6b32cb96b1b5cd34de6926d5c84abc4de4619
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-19 21:26:52 +0700

    memperbaiki kodingan yang error

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,12 @@
+<?php 	
+
+		class Mahasiswa extends CI_Controller {
+			public function index()
+			{
+				$this->load->view('templates/header');
+				$this->load->view('mahasiswa/index');
+				$this->load->view('templates/footer');
+			}
+		}
+
+ ?>
--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,11 +1,10 @@
 <div class="container">
-	<div class="list-group">
-  <a href="#" class="list-group-item list-group-item-action active">
-    Cras justo odio
-  </a>
-  <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-  <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-  <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-  <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Vestibulum at eros</a>
-</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+		<ul class="list-group">
+			<li class="list-group-item">Cras Justro odio</li>	
+		</ul>
+		</div>
+	</div>
 </div>
