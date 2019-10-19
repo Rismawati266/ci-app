1361d0a691599719ab762fb2d67d3586fd69ba3b
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-19 08:23:47 +0700

    mengubah nama judul halaman website

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewpoort" content="width-device-width, initial-scale-1.0">
-	<meta http-equiv="X-UA-Comptaible" content="ie-edge">
-	<title>Home / Index</title>
-</head>
-<body>
-	<h1>Home / Index</h1>
-</body>
-</html>
+
+    <h1>Hello, world!</h1>
