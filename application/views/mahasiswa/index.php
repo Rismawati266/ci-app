b91f05b4300a3b06799dd6a33284ee513a93b1d6
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-20 01:35:23 +0700

    memperbaiki kodingan yang error dan menambahkan file Mahasiswa_model

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -4,7 +4,9 @@
 		<div class="col-md-6">
 			<h3>Daftar Mahasiswa</h3>
 			<ul class="list-group">
-				<li class="list-group-item">Cras Justro odio</li>	
+				<?php foreach( $mahasiswa as $mhs ) : ?>
+				<li class="list-group-item"><?= $mhs['nama']; ?></li>	
+				<?php endforeach; ?>
 			</ul>
 		</div>
 	</div>
