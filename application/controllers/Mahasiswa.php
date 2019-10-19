c82d1333db7fe7058fbf82dc2214d41da95783f4
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-19 21:35:51 +0700

    mengaktifkan database di semua kelas

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,14 +1,22 @@
 <?php 	
 
 		class Mahasiswa extends CI_Controller {
+			
+			public function __construct()
+			{
+				parent::__construct();
+				$this->load->database(); 
+			}
+
 			public function index()
 			{
-				$this->load->database();
 				$data['judul'] = 'Daftar Mahasiswa';
 				$this->load->view('templates/header', $data);
 				$this->load->view('mahasiswa/index', $data);
 				$this->load->view('templates/footer');
 			}
+
+
 		}
 
  ?>
