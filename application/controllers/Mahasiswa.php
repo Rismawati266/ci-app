7f20e315e6ce78d01091109f7ba90e9e94998780
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-19 21:32:27 +0700

    mengaktifkan database

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -3,9 +3,10 @@
 		class Mahasiswa extends CI_Controller {
 			public function index()
 			{
+				$this->load->database();
 				$data['judul'] = 'Daftar Mahasiswa';
 				$this->load->view('templates/header', $data);
-				$this->load->view('mahasiswa/index');
+				$this->load->view('mahasiswa/index', $data);
 				$this->load->view('templates/footer');
 			}
 		}
