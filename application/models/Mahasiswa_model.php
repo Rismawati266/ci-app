07340127a8227778c2b66cdaacaf38a608cf3826
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-20 18:20:18 +0700

    menambahkan pencarian berdasarkan nrp, email, dan jurusan

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -51,6 +51,9 @@
 		{
 			$keyword = $this->input->post('keyword', true);
 			$this->db->like('nama', $keyword);
+			$this->db->or_like('nrp', $keyword);
+			$this->db->or_like('email', $keyword);
+			$this->db->or_like('jurusan', $keyword);
 			return $this->db->get('mahasiswa')->result_array();
 		}
 
