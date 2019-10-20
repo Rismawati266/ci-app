925ec9577ac8fcc2ab02c1cfb3fb9edf31a6420d
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-20 20:34:48 +0700

    membuat perintah untuk menampilkan data peoples

---
+++ application/views/peoples/index.php
@@ -0,0 +1,31 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-10">
+			
+			<h3 class="mt-3">List of Peoples</h3>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nama</th>
+						<th>Email</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>1</th>
+						<td>Rismawati</td>
+						<td>risma.wati266@mail.unpas.ac.id</td>
+						<td>
+							<a href="" class="badge badge-warning">Detail</a>
+							<a href="" class="badge badge-success">Ubah</a>
+							<a href="" class="badge badge-danger">Hapus</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+
+		</div>
+	</div>
+</div>
