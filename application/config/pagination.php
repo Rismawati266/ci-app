6cbf362a2c460c2f1b41a4d6502f9320393f1d5d
Rismawati <risma.wati266@mail.unpas.ac.id>
2019-10-23 01:52:05 +0700

    menambahkan file pagination.php di folder config

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -1,18 +1,5 @@
-<?php 	
+<?php 
 
-		class Peoples extends CI_Controller 
-		{
-
-			public function index()
-			{
-				$data['judul'] = 'List of Peoples';
-
-				$this->load->model('Peoples_model', 'peoples');
-
-				// load library
-				$this->load->library('pagination');
-
-				// config
 				$config['base_url'] = 'http://localhost:8080/ci-app/peoples/index';
 				$config['total_rows'] = $this->peoples->countAllPeoples();
 				$config['per_page'] = 8;
@@ -46,19 +33,3 @@
 
 				$config['attributes'] = array('class' => 'page-link');
 
-
-
-				// Initialzie
-				$this->pagination->initialize($config);
-
-
-
-				$data['start'] = $this->uri->segment(3);
-				$data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start']);
-
-				$this->load->view('templates/header', $data);
-				$this->load->view('peoples/index', $data);
-				$this->load->view('templates/footer');
-			}
-
-		}
--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -5,7 +5,7 @@
 		<div class="col-md-5">
 			<form action="<?= base_url('peoples'); ?>" method="post">
 				<div class="input-group mb-3">
-  					<input type="text" class="form-control" placeholder="Search Keyword . . ." name="keyword" autocomplete="off" autofocus="">
+  					<input type="text" class="form-control" placeholder="Search Keyword . . ." name="keyword" autocomplete="off" autofocus>
   					<div class="input-group-append">
   						<input class="btn btn-primary" type="submit" name="submit">
   					</div>
